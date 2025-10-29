a74902ac32490c52841607e3250ec1cf04d4054b
wlingxiao <5276935+wlingxiao@users.noreply.github.com>
2025-10-29 15:02:48 +0800

    Remove resilio-sync

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,20 +34,5 @@ EOF
 dnf5 install --setopt=install_weak_deps=False -y code
 sed -i 's@enabled=1@enabled=0@g' "/etc/yum.repos.d/vscode.repo"
 
-# Resilio Sync
-# https://help.resilio.com/hc/en-us/articles/206178924-Installing-Sync-package-on-Linux
-tee /etc/yum.repos.d/resilio-sync.repo <<'EOF'
-[resilio-sync]
-name=Resilio Sync
-baseurl=https://linux-packages.resilio.com/resilio-sync/rpm/\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://linux-packages.resilio.com/resilio-sync/key.asc
-EOF
-dnf5 install --setopt=install_weak_deps=False -y resilio-sync
-sed -i 's@enabled=1@enabled=0@g' "/etc/yum.repos.d/resilio-sync.repo"
-# https://aur.archlinux.org/cgit/aur.git/tree/rslsync_user.service?h=rslsync
-sed -i 's@WantedBy=multi-user.target@WantedBy=default.target@g' "/usr/lib/systemd/user/resilio-sync.service"
-
 /ctx/fix-opt.sh
 /ctx/cleanup.sh
